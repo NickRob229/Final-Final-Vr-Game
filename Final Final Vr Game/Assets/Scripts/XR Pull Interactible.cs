4c2923e28861479c46ebfb0a4a9d80db91d47f8c
NickRob229 <553594@chrome.escg.ac.uk>
2025-03-12 16:19:46 +0000

    stuff before closing for trying to get the errors for visual scripting to show

---
+++ Final Final Vr Game/Assets/Scripts/XR Pull Interactible.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+namespace UnityEngine.XR.Interaction.Toolkit.Interactables
+{
+    public class XRPullInteractible : XRBaseInteractables
+    {
+        aaaa
+    }
+
+}
